7098f3b29c03c620493deb57a122064dfc0feeb9
Wemerson Dias <wemersond64@gmail.com>
2026-03-02 22:00:44 -0300

    deleting

--- src/app/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app.config';
-import { DashboardComponent } from './features/dashboard/dashboard.component';
-
-bootstrapApplication(DashboardComponent, appConfig)
-  .catch((err) => console.error(err));
